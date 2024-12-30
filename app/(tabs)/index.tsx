be83a78ff914cb1a1ec6952e195b65ab058e57c0
Bhuwan-Darai <dr.darai29@gmail.com>
2024-12-30 19:42:57 +0545

    fixed the signup with google using web uri's Agent error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,133 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Platform, View, Text, Button } from "react-native";
+import { WebView } from "react-native-webview";
+import { useCameraPermissions } from "expo-camera";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Location from "expo-location";
+import { get } from "http";
 
 export default function HomeScreen() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [error, setError] = useState<{
+    code: number;
+    description: string;
+  } | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  console.log("Permission", permission);
+
+  useEffect(() => {
+    const getLocationPermission = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    };
+
+    getLocationPermission();
+  }, []);
+
+  if ((!permission?.granted && !location) || errorMsg) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.permissionContainer}>
+          <StatusBar style="dark" />
+          <Text style={styles.message}>
+            We need your permission to show the camera
+          </Text>
+          <Button
+            onPress={() => requestPermission()}
+            title="Grant Permission"
+          />
+          <Button
+            onPress={async () => {
+              const { status } =
+                await Location.requestForegroundPermissionsAsync();
+              if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+              }
+
+              let location = await Location.getCurrentPositionAsync({});
+              setLocation(location);
+            }}
+            title="Grant Location Permission"
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        {error ? (
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>Error: {error.description}</Text>
+          </View>
+        ) : (
+          <WebView
+            style={styles.webview}
+            source={{
+              uri: "https://www.ichhihana.com.np/login",
+              // headers: {
+              //   "User-Agent":
+              //     "Mozilla/133.0.3 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.200 Safari/537.36",
+              // },
+            }}
+            onError={(syntheticEvent) => {
+              const { nativeEvent } = syntheticEvent;
+              setError(nativeEvent);
+            }}
+            // onShouldStartLoadWithRequest={(request) => {
+            //   if (request.url.includes("accounts.google.com")) {
+            //     WebBrowser.openBrowserAsync(request.url);
+            //     return false;
+            //   }
+            //   return true;
+            // }}
+            domStorageEnabled={true}
+            sharedCookiesEnabled={true}
+            userAgent="Mozilla/133.0.3 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.200 Safari/537.36"
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  permissionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  container: {
+    flex: 1,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  webview: {
+    flex: 1,
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    fontSize: 16,
+    color: "red",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
   },
 });
